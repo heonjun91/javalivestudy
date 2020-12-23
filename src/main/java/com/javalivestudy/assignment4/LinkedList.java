8e63baefefcce06f99c9c843200b7d73c791a900
Heonjun Jeong <gjswns1991@gmail.com>
2020-12-23 15:20:08 +0900

    인텔리제이 깃연동

--- src/main/java/com/javalivestudy/assignment4/LinkedList.java
+++ src/main/java/com/javalivestudy/assignment4/LinkedList.java
@@ -1,4 +1,76 @@
 package com.javalivestudy.assignment4;
 
+import javax.swing.text.Position;
+
 public class LinkedList {
+
+    private ListNode head;
+
+    public LinkedList(ListNode head) {
+        this.head = head;
+    }
+
+    public ListNode add(ListNode head, ListNode nodeToAdd, int position){
+        ListNode tempNode= head;
+
+        if(position<1){
+            //추가실패
+            System.out.println("position은 0보다 커야함");
+            return null;
+        }
+
+        for(int i=0; i<position; i++){
+            if(tempNode.getNextNode()==null){ // position > size 이면 제일 뒤로 추가
+                break;
+            }
+            tempNode = tempNode.getNextNode();
+        }
+
+        nodeToAdd.setNextNode(tempNode.getNextNode());
+        tempNode.setNextNode(nodeToAdd);
+
+        return nodeToAdd;
+    }
+
+    public ListNode remove(ListNode head, int positionToRemove){
+        ListNode nodeToRemove = head;
+        ListNode prevNode = null;
+
+        if(positionToRemove <1){
+            //삭제실패
+            System.out.println("position은 0보다 커야함");
+            return null;
+        }
+
+        for(int i=0; i<positionToRemove; i++){
+            if(nodeToRemove.getNextNode()==null){
+                //삭제실패
+                System.out.println("해당 위치에 삭제할 노드가 없음");
+                return null;
+            }
+            prevNode = nodeToRemove;
+            nodeToRemove = nodeToRemove.getNextNode();
+        }
+
+        prevNode.setNextNode(nodeToRemove.getNextNode());
+
+        return nodeToRemove;
+    }
+
+    public boolean contains(ListNode head, ListNode nodeTocheck){
+        ListNode tempNode = head;
+
+        while(true){
+            if(tempNode.equals(nodeTocheck)) {
+                return true;
+            }
+            else{
+                if(tempNode.getNextNode()==null){
+                    return false;
+                }
+                tempNode = tempNode.getNextNode();
+            }
+        }
+    }
+
 }
--- src/test/java/com/javalivestudy/assignment4/LinkedListTest.java
+++ src/test/java/com/javalivestudy/assignment4/LinkedListTest.java
@@ -1,7 +1,30 @@
 package com.javalivestudy.assignment4;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
 
+    @Test
+    void add() {
+        ListNode head = new ListNode(0,null);
+        LinkedList list = new LinkedList(head);
+
+        ListNode newNode = new ListNode(1,null);
+
+        //position이 size보다 클경우 null 반환 (추가 실패)
+        assertNull(list.add(head,newNode,0));
+
+        //추가 성공시 추가한 노드 반환
+        assertEquals(newNode,list.add(head,newNode,1));
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void contains() {
+    }
 }
