e66797ef18d74556a263524f253c4c953f6381cd
Heonjun Jeong <gjswns1991@gmail.com>
2020-12-23 16:03:07 +0900

    4주차 LinkedListTest.java

--- src/test/java/com/javalivestudy/assignment4/LinkedListTest.java
+++ src/test/java/com/javalivestudy/assignment4/LinkedListTest.java
@@ -22,9 +22,32 @@ class LinkedListTest {
 
     @Test
     void remove() {
+        ListNode head = new ListNode(0,null);
+        LinkedList list = new LinkedList(head);
+
+        //position : head -> 1 -> 2-> 3-> 4-> 5 세팅
+        //data     : head -> 10->20->30->40->50 세팅
+        for(int i=0; i<5; i++){
+            ListNode newNode = new ListNode((i+1)*10, null);
+            list.add(head,newNode,i+1);
+        }
+
+        //position 2 인 노드를 삭제, 삭제된 노드의 데이터값(20)이 맞는지 확인
+        assertEquals(20,list.remove(head,2).getData());
+
+        //new position : head -> 1 -> 2-> 3-> 4-> 5 세팅
+        //new data     : head -> 10->30->40->50->null 세팅
+
+
+        //삭제실패 (position>0) , null반환
+        assertNull(list.remove(head,0));
+
+        //삭제실패 (하나가 삭제됐으므로 position 5엔 해당노드가 없음)
+        assertNull(list.remove(head,5));
     }
 
-    @Test
+    @Test 
     void contains() {
+        // todo...
     }
 }
